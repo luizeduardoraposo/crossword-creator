f478f6f3a442b503401db4db2d798e45fc09c7b7
Luiz Eduardo Raposo <luizeduardoraposo@protonmail.com>
2025-10-01 03:35:06 -0300

    Move 4x4 letter matrices from matrix.generated.js to matrix4x4.js

--- matrix4x4.js
+++ matrix4x4.js
@@ -2,3 +2,7 @@
 // Data: 2025-10-01T06:31:45.618Z
 const MATRICES = {};
 module.exports = MATRICES;
+
+MATRICES['matrix4x4-1'] = ['D', 'T', 'S', 'N', 'T', 'E', 'E', 'M', 'O', 'A', 'A', 'N', 'M', 'A', 'I', 'T'];
+MATRICES['matrix4x4-2'] = ['M', 'S', 'N', 'D', 'N', 'S', 'I', 'D', 'N', 'C', 'M', 'I', 'S', 'S', 'M', 'R'];
+MATRICES['matrix4x4-3'] = ['N', 'D', 'O', 'D', 'C', 'E', 'U', 'C', 'T', 'I', 'D', 'I', 'C', 'C', 'M', 'O'];
