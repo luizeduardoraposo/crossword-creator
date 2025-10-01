400f15fbc985f20f24e1c4afcd50384eedc82615
Luiz Eduardo Raposo <luizeduardoraposo@protonmail.com>
2025-10-01 03:31:17 -0300

    Add 5x5 letter matrices to matrix5x5.js

---
+++ matrix5x5.js
@@ -0,0 +1,10 @@
+// Arquivo gerado automaticamente por matrix.js (acumulador)
+// Data: 2025-10-01T06:18:59.057Z
+const MATRICES = {};
+module.exports = MATRICES;
+MATRICES['matrix5x5-1'] = ['D', 'S', 'E', 'C', 'C', 'O', 'C', 'O', 'R', 'N', 'T', 'I', 'D', 'O', 'T', 'I', 'C', 'C', 'S', 'R', 'N', 'C', 'N', 'N', 'R'];
+MATRICES['matrix5x5-2'] = ['M', 'U', 'E', 'M', 'O', 'E', 'N', 'O', 'A', 'E', 'A', 'T', 'I', 'N', 'U', 'S', 'C', 'I', 'N', 'A', 'E', 'D', 'I', 'A', 'O'];
+MATRICES['matrix5x5-3'] = ['A', 'N', 'D', 'M', 'R', 'O', 'S', 'O', 'O', 'U', 'E', 'O', 'S', 'N', 'U', 'C', 'R', 'N', 'T', 'E', 'S', 'D', 'I', 'A', 'I'];
+MATRICES['matrix5x5-4'] = ['A', 'S', 'M', 'O', 'O', 'E', 'O', 'S', 'N', 'O', 'D', 'O', 'M', 'I', 'N', 'C', 'U', 'S', 'I', 'E', 'R', 'D', 'A', 'I', 'A'];
+MATRICES['matrix5x5-5'] = ['N', 'R', 'N', 'E', 'A', 'S', 'M', 'D', 'E', 'C', 'S', 'A', 'E', 'A', 'R', 'U', 'D', 'M', 'E', 'T', 'C', 'M', 'O', 'O', 'O'];
+MATRICES['matrix5x5-6'] = ['I', 'T', 'N', 'R', 'N', 'E', 'S', 'D', 'S', 'E', 'M', 'N', 'O', 'T', 'T', 'C', 'S', 'E', 'E', 'D', 'U', 'N', 'T', 'I', 'A'];
